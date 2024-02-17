2bf1da2a13d07b4bb1c39b7e5d86bbd2b03d84b6
pirate2580 <90294883+pirate2580@users.noreply.github.com>
2024-02-17 18:09:25 -0500

    add model to server.py

--- backend/server.py
+++ backend/server.py
@@ -1,9 +1,26 @@
 from flask import Flask, jsonify
 import serial
 from flask_cors import CORS
+import tensorflow as tf
 
 app = Flask(__name__)
 CORS(app)
+
+
+model = tf.keras.Sequential([
+    tf.keras.layers.Reshape((28, 28, 1), input_shape=(28, 28)),  # Reshape input images to (28, 28, 1)
+    tf.keras.layers.Conv2D(32, (3, 3), activation='relu', input_shape=(28, 28, 1)),
+    tf.keras.layers.MaxPooling2D((2, 2)),
+    tf.keras.layers.Conv2D(64, (3, 3), activation='relu'),
+    tf.keras.layers.MaxPooling2D((2, 2)),
+    tf.keras.layers.Conv2D(64, (3, 3), activation='relu'),
+    tf.keras.layers.Flatten(),
+    tf.keras.layers.Dense(128, activation='relu'),
+    tf.keras.layers.Dense(62, activation='softmax')  # 62 classes for multiclass classification
+])
+
+model.load_weights('../emnist_model.h5')
+
 
 @app.route("/api/home", methods=['GET'])
 def return_home():
