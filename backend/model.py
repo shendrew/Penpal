ec3fd00ac88a8d049edc3ee3e53127fa6563cdb4
TianYao12 <110309420+TianYao12@users.noreply.github.com>
2024-02-17 18:30:51 -0500

    failed to run model

--- backend/model.py
+++ backend/model.py
@@ -1,11 +1,5 @@
-from flask import Flask, jsonify
-# import serial
-from flask_cors import CORS
 import tensorflow as tf
 import numpy as np
-
-app = Flask(__name__)
-CORS(app)
 
 model = tf.keras.Sequential([
     tf.keras.layers.Reshape((28, 28, 1), input_shape=(28, 28)),  # Reshape input images to (28, 28, 1)
@@ -21,22 +15,4 @@ model = tf.keras.Sequential([
 
 model.load_weights('../emnist_model.h5')
 
-@app.route("/api/home", methods=['GET'])
-def return_home():
-    return jsonify({
-        "message":"hello world"
-    })
-
-@app.route('/process_data', methods=['GET'])
-def process_data():
-    # read from serial monitor
-    ser = serial.Serial('COM3', 9600)
-    data = ser.readline()
-
-    # put data through neural netwrok
-    # processed_data = ...
-
-    #return jsonify(processed_data)
-
-if __name__ == "__main__":
-    app.run(debug=True, port=8080)
+print(model.predict(np.random.rand(28,28,1)))
